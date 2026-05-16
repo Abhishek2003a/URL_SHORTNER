60f0b051768de3916a81f37796bda4255569132d
Abhishek Kumar <Abhishekprajapati2003sanjaykp@gmail.com>
2026-05-17 00:27:39 +0530

    feat(auth): add signup, login, database schema, and routes

---
+++ Backend/src/controller/url.controller.js
@@ -0,0 +1,51 @@
+const Url = require("../model/urlsSchema");
+
+const getAllURLs = async (req, res) => {
+  try {
+    const userId = req.userId;
+    const urls = await Url.find({
+      userId,
+    });
+    res.status(200).json({ urls });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const putShortURL = async (req, res) => {
+  try {
+    const { originalURL } = req.query;
+    if (!originalURL) {
+      return res.status(400).json({ message: "Original URL is required" });
+    }
+    const shortCode = Math.random().toString(36).substring(2, 8);
+    const userId = req.userId;
+    await Url.create({ originalURL, shortCode, userId });
+    res
+      .status(201)
+      .json({
+        message: "Short URL created successfully",
+        shortURL: `${req.protocol}://${req.get("host")}/${shortCode}`,
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const getOriginalURL = async (req, res) => {
+  try {
+    const { shortCode } = req.params;
+    const url = await Url.findOne({ shortCode });
+    if (!url) {
+      return res.status(404).json({ message: "Short URL not found" });
+    }
+    res.status(200).json({ originalURL: url.originalURL });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+module.exports = { getAllURLs, putShortURL, getOriginalURL };
---
+++ Backend/src/middleware/validateLogin.js
@@ -0,0 +1,7 @@
+module.export=(req,res,next)=>{
+    const {email,password}=req.body;
+    if(!email || !password){
+        return res.status(400).json({message:"Email and Password are required"});
+    }
+    next()
+}
